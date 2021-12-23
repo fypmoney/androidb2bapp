00369f58edadfb5a3ccc060b54ab7d95c1d45b18
Pawankumar <pawan.kumar@fypmoney.in>
2021-12-23 09:12:26 +0530

    release2.0.0
    Signed-off-by: Pawankumar <pawan.kumar@fypmoney.in>

--- app/src/main/java/com/fypmoney/model/YourTaskModel.kt
+++ app/src/main/java/com/fypmoney/model/YourTaskModel.kt
@@ -1,6 +1,8 @@
 package com.fypmoney.model
 
+import androidx.annotation.Keep
 
+@Keep
 data class YourTaskModel(var Id: Int, var Relation: String, var Amount: String,
                          var Title: String?) {
     constructor() : this(0, "","","")
--- app/src/main/java/com/fypmoney/view/ordercard/activateofflinecard/CheckKitNumberRequest.kt
+++ app/src/main/java/com/fypmoney/view/ordercard/activateofflinecard/CheckKitNumberRequest.kt
@@ -1,5 +1,8 @@
 package com.fypmoney.view.ordercard.activateofflinecard
 
+import androidx.annotation.Keep
+
+@Keep
 data class CheckKitNumberRequest(
     var kitNumber:String? = null
 )
