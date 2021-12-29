6c8dc16a4b1e1f8f20312328652e40ace808d105
Pawankumar <pawan.kumar@fypmoney.in>
2021-12-29 14:20:05 +0530

    @keep issue in referral screen api.
    Signed-off-by: Pawankumar <pawan.kumar@fypmoney.in>

--- app/src/main/java/com/fypmoney/view/referandearn/model/ReferMessageResponse.kt
+++ app/src/main/java/com/fypmoney/view/referandearn/model/ReferMessageResponse.kt
@@ -1,13 +1,16 @@
 package com.fypmoney.view.referandearn.model
 
-class ReferMessageResponse {
-    val maxReferredCount: Any? = null
-    val refereeAmount = 0
-    val referLine2: String? = null
-    val rfu2: String? = null
-    val referMessage: String? = null
-    val rfu1: String? = null
-    val referrerAmount = 0
-    val referLine1: String? = null
+import androidx.annotation.Keep
+
+@Keep
+data class ReferMessageResponse (
+    val maxReferredCount: Any? = null,
+    val refereeAmount:Int = 0,
+    val referLine2: String? = null,
+    val rfu2: String? = null,
+    val referMessage: String? = null,
+    val rfu1: String? = null,
+    val referrerAmount:Int = 0,
+    val referLine1: String? = null,
     val rfu3: Any? = null
-}
+)
