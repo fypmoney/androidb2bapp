ac9a21bc10278afdb6d8a430536b73b687564431
Pawankumar <pawan.kumar@fypmoney.in>
2022-06-24 12:52:18 +0530

    # Insights feature 1.Insights class is created.
    Signed-off-by: Pawankumar <pawan.kumar@fypmoney.in>

---
+++ app/src/main/java/com/fypmoney/view/insights/view/InsightsFragment.kt
@@ -0,0 +1,50 @@
+package com.fypmoney.view.insights.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.fypmoney.BR
+import com.fypmoney.R
+import com.fypmoney.base.BaseFragment
+import com.fypmoney.databinding.FragmentInsightsBinding
+import com.fypmoney.view.insights.viewholder.InsightsFragmentVM
+
+class InsightsFragment : BaseFragment<FragmentInsightsBinding,InsightsFragmentVM>() {
+
+    companion object {
+        fun newInstance() = InsightsFragment()
+    }
+
+
+    private  val insightsFragmentVM by viewModels<InsightsFragmentVM> { defaultViewModelProviderFactory }
+    private lateinit var binding: FragmentInsightsBinding
+
+
+    override fun onTryAgainClicked() {
+    }
+
+    /**
+     * Override for set binding variable
+     *
+     * @return variable id
+     */
+    override fun getBindingVariable(): Int  = BR.viewModel
+
+    /**
+     * @return layout resource id
+     */
+    override fun getLayoutId(): Int =R.layout.fragment_insights
+
+    /**
+     * Override for set view model
+     *
+     * @return view model instance
+     */
+    override fun getViewModel(): InsightsFragmentVM  = insightsFragmentVM
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = getViewDataBinding()
+    }
+
+}
---
+++ app/src/main/java/com/fypmoney/view/insights/viewholder/InsightsFragmentVM.kt
@@ -0,0 +1,7 @@
+package com.fypmoney.view.insights.viewholder
+
+import android.app.Application
+import com.fypmoney.base.BaseViewModel
+
+class InsightsFragmentVM(application: Application) : BaseViewModel(application) {
+}
