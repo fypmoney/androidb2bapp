acba8831a29fa8d400b3841677701b2a94d62bd7
Tapan Yadav <tapan.yadav@fypmoney.in>
2022-07-19 16:43:31 +0530

    Leaderboard ui design implementation

---
+++ app/src/main/java/com/fypmoney/view/arcadegames/ui/LeaderBoardFragment.kt
@@ -0,0 +1,19 @@
+package com.fypmoney.view.arcadegames.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fypmoney.R
+
+class LeaderBoardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leader_board, container, false)
+    }
+}
