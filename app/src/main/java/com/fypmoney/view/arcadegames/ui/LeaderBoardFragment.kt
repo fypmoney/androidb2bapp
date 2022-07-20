da62a0ff1a83ed546015b0c25128e7ccf5c50a1d
Tapan Yadav <tapan.yadav@fypmoney.in>
2022-07-20 11:12:53 +0530

    Some image change on top section

--- app/src/main/java/com/fypmoney/view/arcadegames/ui/LeaderBoardFragment.kt
+++ app/src/main/java/com/fypmoney/view/arcadegames/ui/LeaderBoardFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import com.fypmoney.BR
 import com.fypmoney.R
+import com.fypmoney.base.BaseFragment
+import com.fypmoney.bindingAdapters.setBackgroundDrawable
+import com.fypmoney.databinding.FragmentLeaderBoardBinding
+import com.fypmoney.view.arcadegames.viewmodel.FragmentLeaderBoardVM
 
-class LeaderBoardFragment : Fragment() {
+class LeaderBoardFragment : BaseFragment<FragmentLeaderBoardBinding, FragmentLeaderBoardVM>() {
+
+    private val leaderBoardFragmentVM by viewModels<FragmentLeaderBoardVM> { defaultViewModelProviderFactory }
+    private var mViewBinding: FragmentLeaderBoardBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +26,58 @@ class LeaderBoardFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_leader_board, container, false)
     }
+
+    override fun onTryAgainClicked() {}
+
+    private fun setBackgrounds() {
+        mViewBinding?.let {
+            setBackgroundDrawable(
+                it.ivLeaderboardNumber1,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                58f,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                2f,
+                true
+            )
+
+            setBackgroundDrawable(
+                it.ivLeaderboardNumber2,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                58f,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                2f,
+                true
+            )
+
+            setBackgroundDrawable(
+                it.chipTicketView,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                58f,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                0f,
+                false
+            )
+
+            setBackgroundDrawable(
+                it.ivLeaderboardNumber3,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                58f,
+                ContextCompat.getColor(this.requireContext(), R.color.back_surface_color),
+                2f,
+                true
+            )
+
+        }
+    }
+
+    override fun getBindingVariable(): Int {
+        return BR.viewModel
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.fragment_leader_board
+    }
+
+    override fun getViewModel(): FragmentLeaderBoardVM = leaderBoardFragmentVM
+
 }
---
+++ app/src/main/java/com/fypmoney/view/arcadegames/viewmodel/FragmentLeaderBoardVM.kt
@@ -0,0 +1,8 @@
+package com.fypmoney.view.arcadegames.viewmodel
+
+import android.app.Application
+import com.fypmoney.base.BaseViewModel
+
+class FragmentLeaderBoardVM(application: Application) : BaseViewModel(application) {
+
+}
