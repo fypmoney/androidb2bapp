940fe41e458bf60bc0980bb881f301ada9f48288
Pawankumar <pawan.kumar@fypmoney.in>
2022-08-10 19:05:54 +0530

    #Explore recharge deeplink
    Signed-off-by: Pawankumar <pawan.kumar@fypmoney.in>

--- app/src/main/java/com/fypmoney/view/home/main/explore/sectionexplore/SectionExploreFragment.kt
+++ app/src/main/java/com/fypmoney/view/home/main/explore/sectionexplore/SectionExploreFragment.kt
@@ -217,6 +217,41 @@ class SectionExploreFragment :
                                 AppConstants.INSIGHTS -> {
                                     findNavController().navigate(R.id.navigation_insights)
                                 }
+                                AppConstants.PREPAID_RECHARGE_REDIRECTION->{
+                                    findNavController().navigate(Uri.parse("https://www.fypmoney.in/recharge/${AppConstants.PREPAID}"),
+                                        navOptions {
+                                            anim {
+                                                popEnter = R.anim.slide_in_left
+                                                popExit = R.anim.slide_out_righ
+                                                enter = R.anim.slide_in_right
+                                                exit = R.anim.slide_out_left
+                                            }
+                                        })
+                                }
+
+                                AppConstants.POSTPAID_RECHARGE_REDIRECTION->{
+                                    findNavController().navigate(Uri.parse("https://www.fypmoney.in/recharge/${AppConstants.POSTPAID}"),
+                                        navOptions {
+                                            anim {
+                                                popEnter = R.anim.slide_in_left
+                                                popExit = R.anim.slide_out_righ
+                                                enter = R.anim.slide_in_right
+                                                exit = R.anim.slide_out_left
+                                            }
+                                        })
+                                }
+
+                                AppConstants.DTH_RECHARGE_REDIRECTION->{
+                                    findNavController().navigate(Uri.parse("https://www.fypmoney.in/recharge/dth"),
+                                        navOptions {
+                                            anim {
+                                                popEnter = R.anim.slide_in_left
+                                                popExit = R.anim.slide_out_righ
+                                                enter = R.anim.slide_in_right
+                                                exit = R.anim.slide_out_left
+                                            }
+                                        })
+                                }
                                 else -> {
                                     Utility.deeplinkRedirection(
                                         redirectionResources,
@@ -357,6 +392,41 @@ class SectionExploreFragment :
                             }
                             null -> TODO()
                         }
+                    }
+                    AppConstants.PREPAID_RECHARGE_REDIRECTION->{
+                        findNavController().navigate(Uri.parse("https://www.fypmoney.in/recharge/${AppConstants.PREPAID}"),
+                            navOptions {
+                                anim {
+                                    popEnter = R.anim.slide_in_left
+                                    popExit = R.anim.slide_out_righ
+                                    enter = R.anim.slide_in_right
+                                    exit = R.anim.slide_out_left
+                                }
+                            })
+                    }
+
+                    AppConstants.POSTPAID_RECHARGE_REDIRECTION->{
+                        findNavController().navigate(Uri.parse("https://www.fypmoney.in/recharge/${AppConstants.POSTPAID}"),
+                            navOptions {
+                                anim {
+                                    popEnter = R.anim.slide_in_left
+                                    popExit = R.anim.slide_out_righ
+                                    enter = R.anim.slide_in_right
+                                    exit = R.anim.slide_out_left
+                                }
+                            })
+                    }
+
+                    AppConstants.DTH_RECHARGE_REDIRECTION->{
+                        findNavController().navigate(Uri.parse("https://www.fypmoney.in/recharge/dth"),
+                            navOptions {
+                                anim {
+                                    popEnter = R.anim.slide_in_left
+                                    popExit = R.anim.slide_out_righ
+                                    enter = R.anim.slide_in_right
+                                    exit = R.anim.slide_out_left
+                                }
+                            })
                     }
                 }
 
