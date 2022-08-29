69bfb4a03422023d4fe7de7b0c0d15f82c282c18
Tapan Yadav <tapan.yadav@fypmoney.in>
2022-08-29 19:16:41 +0530

    Whatsapp get api implementation

--- app/src/main/java/com/fypmoney/view/whatsappnoti/viewmodel/NotificationSettingsFragmentVM.kt
+++ app/src/main/java/com/fypmoney/view/whatsappnoti/viewmodel/NotificationSettingsFragmentVM.kt
@@ -1,16 +1,8 @@
 package com.fypmoney.view.whatsappnoti.viewmodel
 
 import android.app.Application
-import android.app.Dialog
-import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.view.ViewGroup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.fyp.trackr.models.TrackrEvent
-import com.fyp.trackr.models.trackr
-import com.fypmoney.R
 import com.fypmoney.base.BaseViewModel
 import com.fypmoney.connectivity.ApiConstant
 import com.fypmoney.connectivity.ApiUrl
@@ -21,20 +13,34 @@ import com.fypmoney.connectivity.retrofit.WebApiCaller
 import com.fypmoney.model.BaseRequest
 import com.fypmoney.view.whatsappnoti.model.WhatsAppNotificationResponse
 import com.fypmoney.view.whatsappnoti.model.WhatsAppOptData
-import kotlinx.android.synthetic.main.dialog_settings_confirm.*
 
 class NotificationSettingsFragmentVM(application: Application) : BaseViewModel(application) {
 
-    var optStatus: Boolean = true
-
-    val stateOptStatus: LiveData<WhatsAppOptStatusState>
-        get() = _stateOptStatus
-    private val _stateOptStatus = MutableLiveData<WhatsAppOptStatusState>()
+//    var optStatus: Boolean = true
+//
+//    val stateOptStatus: LiveData<WhatsAppOptStatusState>
+//        get() = _stateOptStatus
+//    private val _stateOptStatus = MutableLiveData<WhatsAppOptStatusState>()
 
     val stateOptStatusChange: LiveData<WhatsAppNotificationOptState>
         get() = _stateOptStatusChange
     private val _stateOptStatusChange = MutableLiveData<WhatsAppNotificationOptState>()
 
+    init {
+        getOptStatusApi()
+    }
+
+    private fun getOptStatusApi() {
+        WebApiCaller.getInstance().request(
+            ApiRequest(
+                ApiConstant.API_POST_OPT_STATUS_DATA,
+                NetworkUtil.endURL(ApiConstant.API_POST_OPT_STATUS_DATA) + "init",
+                ApiUrl.GET,
+                BaseRequest(),
+                this, isProgressBar = true
+            )
+        )
+    }
 
     fun callChangeOptStatusApi(code: String?) {
         WebApiCaller.getInstance().request(
@@ -71,11 +77,11 @@ class NotificationSettingsFragmentVM(application: Application) : BaseViewModel(a
         }
     }
 
-    sealed class WhatsAppOptStatusState {
-        object Loading : WhatsAppOptStatusState()
-        data class Error(var errorResponseInfo: ErrorResponseInfo) : WhatsAppOptStatusState()
-//        data class Success(var ) : WhatsAppOptStatusState()
-    }
+//    sealed class WhatsAppOptStatusState {
+//        object Loading : WhatsAppOptStatusState()
+//        data class Error(var errorResponseInfo: ErrorResponseInfo) : WhatsAppOptStatusState()
+//        data class Success(var whatsAppOptData: WhatsAppOptData) : WhatsAppOptStatusState()
+//    }
 
     sealed class WhatsAppNotificationOptState {
         object Loading : WhatsAppNotificationOptState()
